d2626545282a60fe80b217e1d0836031c6cdfeb0
shankace <351339090@qq.com>
2020-01-10 00:00:22 +0800

    添加search方法

--- Link_List/Link.c
+++ Link_List/Link.c
@@ -27,12 +27,18 @@ ProLink Insert(ProLink p, int k, int x){
         p1->Next = NULL;
         return p1;
     }
-    else if(p1->Next){
+    else if(p1->Next && k!=1){
         ProLink tmp = (ProLink)malloc(sizeof(struct Link));
         tmp->Data = x;
         tmp->Next = p1->Next;
         p1->Next = tmp;
         return p;
+    }
+    else if(p1->Next && k==1){
+        ProLink tmp = (ProLink)malloc(sizeof(struct Link));
+        tmp->Next = p1;
+        tmp->Data = x;
+        return tmp;
     }
     else{
         ProLink tmp = (ProLink)malloc(sizeof(struct Link));
@@ -45,7 +51,16 @@ ProLink Insert(ProLink p, int k, int x){
 int Search(ProLink p, int k){
     ProLink p1 = p;
     int i = 0;
-    while()
+    if(!p1 || k<1){
+        return ERROR;
+    } 
+    else{
+        while(i<k-1 && p1->Next){
+            i++;
+            p1 = p1->Next;
+        }
+        return p1->Data;
+    }
 }
 
 
@@ -58,11 +73,8 @@ int main(int argc, char const *argv[])
     p = Insert(p, 3, 0);
     p = Insert(p, 2, 2);
     p = Insert(p, 1, 1);
-    ProLink tmp_1 = p;
-    ProLink tmp_2 = p;
-    printf("%d\n", p->Data);
-    p = p->Next;
-    printf("%d\n", p->Data);
+    int x = Search(p, 2);
+    printf("%d\n", x);
     getchar();
     return 0;
 }
