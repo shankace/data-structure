1d4237e54d532931e5f7ba5eb4fb6b43f8cd1175
shankace <351339090@qq.com>
2020-01-10 18:05:53 +0800

    加入头结点

--- Link_List/Link.c
+++ Link_List/Link.c
@@ -12,39 +12,37 @@ struct Link{
 
 
 ProLink Insert(ProLink p, int k, int x){
-    /* 添加节点时有头尾和中间三种不同情况，并且要保证尾节点的Next指向NULL*/
+    /* p为链表的头指针，初始化的时候为NULL，p链表的尾指针应该指向NULL,
+    添加节点时有头尾和中间三种不同情况。*/
     ProLink p1 = p;
+    ProLink tmp = (ProLink)malloc(sizeof(struct Link));
+    tmp->Next = NULL;
+    tmp->Data = x;
     int i = 0;
+    // 判断链表是否为空
     if(!p1 && k!=1){
+        printf("这个链表为空，不能再第%d处添加元素\n", k);
         return ERROR;
     }
-    while(p1 && i<k-2){
+    if(!p1 && k==1){
+        p1 = (ProLink)malloc(sizeof(struct Link));
+        p1->Next = tmp;
+        return p1;
+    }
+    if(p1 && k==1){
+        p1->Next = tmp;
+        return p1;
+    }
+    while(p1 && i<k-1){
         i++;
         p1 = p1->Next;
     }
-    if(!p1){
-        ProLink p1 = (ProLink)malloc(sizeof(struct Link));
-        p1->Data = x;
-        p1->Next = NULL;  // 尾节点指向NULL
-        return p1;
-    }
-    else if(p1->Next && k!=1){
-        ProLink tmp = (ProLink)malloc(sizeof(struct Link));
-        tmp->Data = x;
-        tmp->Next = p1->Next;
-        p1->Next = tmp;
-        return p;
-    }
-    else if(p1->Next && k==1){
-        ProLink tmp = (ProLink)malloc(sizeof(struct Link));
-        tmp->Next = p1;
-        tmp->Data = x;
-        return tmp;
+    if(!p1->Next || i!=k-1){
+        printf("error");
+        return ERROR;
     }
     else{
-        ProLink tmp = (ProLink)malloc(sizeof(struct Link));
-        tmp->Data = x;
-        tmp->Next = NULL;
+        tmp->Next = p1->Next;
         p1->Next = tmp;
         return p;
     }
@@ -66,33 +64,24 @@ int Search(ProLink p, int k){
 }
 
 ProLink Delete(ProLink p, int k){
+    /* 删除节点的时候应该释放内存 */
     ProLink p1 = p;
     int i = 0;
-    if(!p1 || i<1){
-        return ERROR;
-    }
-    while(p1->Next && i<k-2){
+    while(p1 && i<k-1){
         p1 = p1->Next;
         i++;
-    }
-    if(p1->Next->Next){
-        p1->Next = p1->Next->Next;
-        return p;
-    }
-    else if(p1->Next){
-        p1->Next = NULL;
-        return p;
     }
 }
 
 ProLink Traversal(ProLink p){
     ProLink p1 = p;
     if(!p1){
+        printf("空链表");
         return ERROR;
     }
-    while(p1){
-        printf("%d ", p1->Data);
+    while(p1->Next){
         p1 = p1->Next;
+        printf("%d ", p1->Data);
     }
     printf("\n");
     return p;
@@ -103,11 +92,9 @@ ProLink Traversal(ProLink p){
 int main(int argc, char const *argv[])
 {
     ProLink p=NULL;
-    p = Insert(p, 1, 5);
-    p = Insert(p, 2, 2);
-    p = Insert(p, 3, 0);
-    p = Insert(p, 2, 2);
+    p = Insert(p, 1, 2);
     p = Insert(p, 1, 1);
+    p = Insert(p, 3, 3);
     p = Traversal(p);
     getchar();
     return 0;
