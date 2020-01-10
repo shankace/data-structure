80944e9100423958d669add9ac8b31c536853cb9
shankace <351339090@qq.com>
2020-01-10 14:49:01 +0800

    链表

--- Link_List/Link.c
+++ Link_List/Link.c
@@ -12,6 +12,7 @@ struct Link{
 
 
 ProLink Insert(ProLink p, int k, int x){
+    /* 添加节点时有头尾和中间三种不同情况，并且要保证尾节点的Next指向NULL*/
     ProLink p1 = p;
     int i = 0;
     if(!p1 && k!=1){
@@ -24,7 +25,7 @@ ProLink Insert(ProLink p, int k, int x){
     if(!p1){
         ProLink p1 = (ProLink)malloc(sizeof(struct Link));
         p1->Data = x;
-        p1->Next = NULL;
+        p1->Next = NULL;  // 尾节点指向NULL
         return p1;
     }
     else if(p1->Next && k!=1){
@@ -43,6 +44,7 @@ ProLink Insert(ProLink p, int k, int x){
     else{
         ProLink tmp = (ProLink)malloc(sizeof(struct Link));
         tmp->Data = x;
+        tmp->Next = NULL;
         p1->Next = tmp;
         return p;
     }
@@ -63,6 +65,39 @@ int Search(ProLink p, int k){
     }
 }
 
+ProLink Delete(ProLink p, int k){
+    ProLink p1 = p;
+    int i = 0;
+    if(!p1 || i<1){
+        return ERROR;
+    }
+    while(p1->Next && i<k-2){
+        p1 = p1->Next;
+        i++;
+    }
+    if(p1->Next->Next){
+        p1->Next = p1->Next->Next;
+        return p;
+    }
+    else if(p1->Next){
+        p1->Next = NULL;
+        return p;
+    }
+}
+
+ProLink Traversal(ProLink p){
+    ProLink p1 = p;
+    if(!p1){
+        return ERROR;
+    }
+    while(p1){
+        printf("%d ", p1->Data);
+        p1 = p1->Next;
+    }
+    printf("\n");
+    return p;
+}
+
 
 
 int main(int argc, char const *argv[])
@@ -73,8 +108,7 @@ int main(int argc, char const *argv[])
     p = Insert(p, 3, 0);
     p = Insert(p, 2, 2);
     p = Insert(p, 1, 1);
-    int x = Search(p, 2);
-    printf("%d\n", x);
+    p = Traversal(p);
     getchar();
     return 0;
 }
